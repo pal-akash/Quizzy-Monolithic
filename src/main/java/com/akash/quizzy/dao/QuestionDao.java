34f06354ec1f79cc9b870c2c7d5db1aff4df6677
Akash Pal <108969268+pal-akash@users.noreply.github.com>
2023-08-24 21:11:05 +0530

    [feat] get all questions

---
+++ src/main/java/com/akash/quizzy/dao/QuestionDao.java
@@ -0,0 +1,14 @@
+package com.akash.quizzy.dao;
+
+import com.akash.quizzy.Question;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface QuestionDao extends JpaRepository<Question, Integer> {
+
+
+
+}
--- src/main/java/com/akash/quizzy/service/QuestionService.java
+++ src/main/java/com/akash/quizzy/service/QuestionService.java
@@ -2,10 +2,18 @@ package com.akash.quizzy.service;
 
 import com.akash.quizzy.Question;
 import com.akash.quizzy.dao.QuestionDao;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 
 @Service
 public class QuestionService {
 
+    @Autowired
+    QuestionDao questionDao;
+
+    public List<Question> getAllQuestions() {
+        return questionDao.findAll();
+    }
 }
