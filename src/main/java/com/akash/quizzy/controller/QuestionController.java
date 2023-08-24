3424e35171368581193d539be7096737278ecf8a
Akash Pal <108969268+pal-akash@users.noreply.github.com>
2023-08-24 21:10:42 +0530

    add service and dao layer

---
+++ src/main/java/com/akash/quizzy/controller/QuestionController.java
@@ -0,0 +1,23 @@
+package com.akash.quizzy.controller;
+
+import com.akash.quizzy.Question;
+import com.akash.quizzy.service.QuestionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("question")
+public class QuestionController {
+
+    @Autowired
+    QuestionService questionService;
+
+    @GetMapping("allQuestion")
+    public List<Question> getAllQuestion(){
+        return questionService.getAllQuestions();
+    }
+}
---
+++ src/main/java/com/akash/quizzy/service/QuestionService.java
@@ -0,0 +1,11 @@
+package com.akash.quizzy.service;
+
+import com.akash.quizzy.Question;
+import com.akash.quizzy.dao.QuestionDao;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class QuestionService {
+
+}
