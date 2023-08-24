f90f599350585b76a1ebae0c1684ea91cc638577
Akash Pal <108969268+pal-akash@users.noreply.github.com>
2023-08-24 20:25:03 +0530

    connect postgresql

---
+++ src/main/java/com/akash/quizzy/QuestionController.java
@@ -0,0 +1,15 @@
+package com.akash.quizzy;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("question")
+public class QuestionController {
+
+    @GetMapping("allQuestion")
+    public String getAllQuestion(){
+        return "All questions";
+    }
+}
