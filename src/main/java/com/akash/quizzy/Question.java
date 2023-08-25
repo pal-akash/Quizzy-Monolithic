f6f912446365e9d59f8e9659f4159da708313885
Akash Pal <108969268+pal-akash@users.noreply.github.com>
2023-08-25 18:33:26 +0530

    [feat] fetch question by category

--- src/main/java/com/akash/quizzy/Question.java
+++ src/main/java/com/akash/quizzy/Question.java
@@ -1,9 +1,6 @@
 package com.akash.quizzy;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Data;
 
 @Data
@@ -11,8 +8,9 @@ import lombok.Data;
 public class Question {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
     private String questionTitle;
     private String option1;
     private String option2;
--- src/main/java/com/akash/quizzy/controller/QuestionController.java
+++ src/main/java/com/akash/quizzy/controller/QuestionController.java
@@ -3,9 +3,7 @@ package com.akash.quizzy.controller;
 import com.akash.quizzy.Question;
 import com.akash.quizzy.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -20,4 +18,12 @@ public class QuestionController {
     public List<Question> getAllQuestion(){
         return questionService.getAllQuestions();
     }
+
+    @GetMapping("category/{category}")
+    public List<Question> getQuestionsByCategory(@PathVariable String category){
+        return questionService.getQuestionsByCategory(category);
+    }
+
+
+
 }
