8817913d772ebc11d6518c6460d7fa3107fb6642
Akash Pal <108969268+pal-akash@users.noreply.github.com>
2023-08-27 00:06:55 +0530

    create quiz entity and controller

---
+++ src/main/java/com/akash/quizzy/controller/QuizController.java
@@ -0,0 +1,15 @@
+package com.akash.quizzy.controller;
+
+import com.akash.quizzy.service.QuizService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("quiz")
+public class QuizController {
+
+}
