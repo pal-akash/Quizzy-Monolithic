6e1bc9bf1e43768beb165679e28ce8cd82c6784f
Akash Pal <108969268+pal-akash@users.noreply.github.com>
2023-08-27 19:18:43 +0530

    fetch quiz

--- src/main/java/com/akash/quizzy/controller/QuizController.java
+++ src/main/java/com/akash/quizzy/controller/QuizController.java
@@ -1,12 +1,13 @@
 package com.akash.quizzy.controller;
 
+import com.akash.quizzy.model.Question;
+import com.akash.quizzy.model.QuestionWrapper;
 import com.akash.quizzy.service.QuizService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("quiz")
@@ -18,5 +19,10 @@ public class QuizController {
     @PostMapping("create")
     public ResponseEntity<String> createQuiz(@RequestParam String category, @RequestParam Integer numQ, @RequestParam String title){
         return quizService.createQuiz(category, numQ, title);
+    }
+
+    @GetMapping("get/{id}")
+    public ResponseEntity<List<QuestionWrapper>> getQuiz(@PathVariable Long id){
+        return quizService.getQuiz(id);
     }
 }
--- src/main/java/com/akash/quizzy/service/QuizService.java
+++ src/main/java/com/akash/quizzy/service/QuizService.java
@@ -3,19 +3,22 @@ package com.akash.quizzy.service;
 import com.akash.quizzy.dao.QuestionDao;
 import com.akash.quizzy.dao.QuizDao;
 import com.akash.quizzy.model.Question;
+import com.akash.quizzy.model.QuestionWrapper;
 import com.akash.quizzy.model.Quiz;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class QuizService {
 
     @Autowired
-    QuizDao quizdao;
+    QuizDao quizDao;
 
     @Autowired
     QuestionDao questionDao;
@@ -29,8 +32,21 @@ public class QuizService {
         quiz.setTitle(title);
         quiz.setQuestionsList(questionsForQuiz);
 
-        quizdao.save(quiz);
+        quizDao.save(quiz);
 
         return new ResponseEntity<>("Success", HttpStatus.CREATED);
+    }
+
+    public ResponseEntity<List<QuestionWrapper>> getQuiz(Long id) {
+        Optional<Quiz> quiz = quizDao.findById(id);
+        List<Question> questionsFromDB = quiz.get().getQuestionsList();
+        List<QuestionWrapper> questionsForUser = new ArrayList<>();
+
+        for(Question q : questionsFromDB){
+            QuestionWrapper qw = new QuestionWrapper(q.getId(), q.getQuestionTitle(), q.getOption1(), q.getOption2(), q.getOption3(), q.getOption4());
+            questionsForUser.add(qw);
+        }
+
+        return new ResponseEntity<>(questionsForUser, HttpStatus.OK);
     }
 }
